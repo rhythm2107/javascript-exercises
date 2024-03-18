bf3b223a7787f030c38a43c149d3880df0bbbb4e
Kamil Krzywda <kamil.krzywda.21@gmail.com>
2024-03-18 22:42:27 +0100

    Add solution to exercise JS 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversedStr = string.split('').reverse().join('')
+    return reversedStr
 };
 
 // Do not edit below this line

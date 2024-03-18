42a804bb91e46abfc62c96e9f0e3390d85ed8d25
Kamil Krzywda <kamil.krzywda.21@gmail.com>
2024-03-19 00:08:21 +0100

    Add solution to JS exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    let sum = 0;
+    let startPoint = Math.min(num1, num2);
+    let endPoint = Math.max(num1, num2);
+
+    if (typeof(num1) !== typeof(num2)) {
+        return 'ERROR'
+    } else if (num1 < 0 || num2 < 0) {
+        return 'ERROR'
+    }
+
+    while (startPoint <= endPoint) {
+        sum += startPoint
+        startPoint++
+    }
+    return sum
 };
 
 // Do not edit below this line

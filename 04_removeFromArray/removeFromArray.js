c225a6b821e22b47341f423c4479e317be18d7a8
Kamil Krzywda <kamil.krzywda.21@gmail.com>
2024-03-18 23:30:34 +0100

    Add solution for JS 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
 
+    for (let i = array.length -1; i >= 0; i--) {
+        if (remove.includes(array[i])) {
+            array.splice(i, 1);
+        }
+    }
+    return array
 };
 
 // Do not edit below this line

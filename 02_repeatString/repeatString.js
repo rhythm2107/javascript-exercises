60edad0d1fab1dec9d01c9bf3fe03ce1f76cd1b7
Kamil Krzywda <kamil.krzywda.21@gmail.com>
2024-04-24 00:17:27 +0200

    Add solution to exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,8 @@
-const repeatString = function(string, num) {
-    if (num < 0) return "ERROR";
+const repeatString = (string, num) => {
     let result = ''
+    if (num < 0) {
+        return 'ERROR'
+    }
     for (let i = 0; i < num; i++) {
         result += string
     }

3e15379ceb4c925457f773f5793af3e79e1233e9
Kamil Krzywda <kamil.krzywda.21@gmail.com>
2024-04-25 13:08:17 +0200

    Add solution to sumAll exericse 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,21 +1,23 @@
-const sumAll = function(num1, num2) {
+const sumAll = (start, end) => {
 
-    let sum = 0;
-    let startPoint = Math.min(num1, num2);
-    let endPoint = Math.max(num1, num2);
-
-    if (typeof(num1) !== typeof(num2)) {
-        return 'ERROR'
-    } else if (num1 < 0 || num2 < 0) {
+    if (start < 0 || end < 0) {
         return 'ERROR'
     }
 
-    while (startPoint <= endPoint) {
-        sum += startPoint
-        startPoint++
+    if (typeof start !== 'number' || typeof end !== 'number') {
+        return 'ERROR'
     }
-    return sum
-};
+
+    let result = 0
+    let first = Math.min(start, end)
+    let last = Math.max(start, end)
+
+    for (let i = first; i <= last; i++) {
+        result += i
+    }
+    return result
+}
+
 
 // Do not edit below this line
 module.exports = sumAll;
